18ac2e24ca7f44a95bb853f0cdd852a7f1125037
mazen320 <m.shaaar@saedubai.com>
2023-06-17 16:48:43 +0400

    Added the text pop ups

--- Assets/Scripts/WaypointSwitcher.cs
+++ Assets/Scripts/WaypointSwitcher.cs
@@ -26,18 +26,46 @@ public class WaypointSwitcher : MonoBehaviour
     bool pickup;
 
     public Score score;
+
+    public GameObject pickupUI;     // Reference to the pickup UI Popup object
+    public GameObject deliveryUI;   // Reference to the delivery UI Popup object
+
+    private bool pickupUIShown = false;
+    private bool deliveryUIShown = false;
+
+    private float popupDuration = 3f;   // Duration in seconds for the popup to stay visible
+    private float popupTimer = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
         waypoint.UpdateWaypoint(pickupPoints[Random.Range(0, pickupPoints.Length)]);
         deliveryComplete = false;
         score = GetComponent<Score>();
+
+        // Disable the UI Popups at the start
+        pickupUI.SetActive(false);
+        deliveryUI.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         DetectReaching();
+
+        // Update the popup timer
+        if (popupTimer > 0f)
+        {
+            popupTimer -= Time.deltaTime;
+
+            // Check if the timer has expired
+            if (popupTimer <= 0f)
+            {
+                // Hide the UI Popups
+                pickupUI.SetActive(false);
+                deliveryUI.SetActive(false);
+            }
+        }
     }
 
     public void DetectReaching()
@@ -45,11 +73,10 @@ public class WaypointSwitcher : MonoBehaviour
         deliverCount = Physics.OverlapSphere(transform.position, maxRange, deliverMask);
         pickupCount = Physics.OverlapSphere(transform.position, maxRange, pickupMask);
 
-
         deliver = deliverCount.Length > 0;
         pickup = pickupCount.Length > 0;
 
-        if (deliver)
+        if (deliver && !deliveryUIShown)
         {
             Debug.Log("YOU REACHED A DELIVERY!");
             waypoint.UpdateWaypoint(pickupPoints[0].gameObject);
@@ -59,12 +86,29 @@ public class WaypointSwitcher : MonoBehaviour
                 score.IncreaseScore();
                 added = true;
             }
+
+            // Enable the delivery UI Popup and start the timer
+            deliveryUI.SetActive(true);
+            popupTimer = popupDuration;
+            deliveryUIShown = true;
+
+            // Disable the pickup UI Popup if it was shown previously
+            pickupUI.SetActive(false);
+            pickupUIShown = false;
         }
-        if (pickup)
+        if (pickup && !pickupUIShown)
         {
             Debug.Log("YOU REACHED A PICKUP!");
             waypoint.UpdateWaypoint(deliveryPoints[0].gameObject);
-        }
 
+            // Enable the pickup UI Popup and start the timer
+            pickupUI.SetActive(true);
+            popupTimer = popupDuration;
+            pickupUIShown = true;
+
+            // Disable the delivery UI Popup if it was shown previously
+            deliveryUI.SetActive(false);
+            deliveryUIShown = false;
+        }
     }
 }
