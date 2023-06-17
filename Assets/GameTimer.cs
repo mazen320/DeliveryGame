a762e25dee1251016ace403a70ec43f170f4bdf5
mazen320 <m.shaaar@saedubai.com>
2023-06-17 13:40:54 +0400

    Added gametimer

--- Assets/GameTimer.cs
+++ Assets/GameTimer.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameTimer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float currentTime = 90;
+    [SerializeField] TextMeshProUGUI timerText;
+    GameObject CountdownTimer;
+
+    //Update is called once per frame
+    private void Start()
     {
-        
+        CountdownTimer = GameObject.Find("CountdownTimer");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+        }
+        else
+        {
+            currentTime = 0;
+        }
+
+        if (CountdownTimer.activeSelf == false)
+        {
+            TimeUI(currentTime);
+        }
+
+
+
     }
+
+    //if (currentTime == 0)
+    //{
+    //    SceneManager.LoadScene("UI.WinScreenProp");
+    //}
+
+
+    void TimeUI(float displayingTime)
+    {
+        if (displayingTime < 0)
+        {
+            displayingTime = 0;
+        }
+
+        float minutes = Mathf.FloorToInt(displayingTime / 60);
+        float seconds = Mathf.FloorToInt(displayingTime % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+    }
+
 }
+
